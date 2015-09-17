b6fc757d448f43977cf32653452edcdcc2bf5715
Alberto Forni <alberto.forn@gmail.com>
2015-09-17 16:38:30 +0100

    add extractSvgContent

--- transform.go
+++ transform.go
@@ -2,7 +2,15 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"strings"
 	"text/template"
+
+	"github.com/opesun/goquery"
 )
 
 const svgSpriteTemplate = "./svgSpriteTemplate.svg"
@@ -24,4 +32,55 @@ func injectIconsIntoSvgTemplate(icons string, tmpl *template.Template) bytes.Buf
 	tmpl.Execute(&output, tc)
 
 	return output
+}
+
+func exists(path string) bool {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+
+	return false
+}
+
+func fetchIcons(srcFolder string) []string {
+	if !exists(srcFolder) {
+		panic(fmt.Sprintf("'%s' folder doesn't exist", srcFolder))
+	}
+
+	files, _ := ioutil.ReadDir(srcFolder)
+
+	var svgFiles []string
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), ".svg") {
+			svgFiles = append(svgFiles, path.Join(srcFolder, file.Name()))
+		}
+	}
+
+	return svgFiles
+}
+
+func getSvg(svgBytes []byte) string {
+	svg := bytes.NewReader(svgBytes)
+	doc, err := goquery.Parse(svg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	icon := doc.Find("symbol").OuterHtml()
+
+	return icon
+}
+
+func extractSvgContent(files []string) string {
+	var svgIcons string
+
+	for _, file := range files {
+
+		dat, _ := ioutil.ReadFile(file)
+
+		svgIcons = svgIcons + "\n" + getSvg(dat)
+	}
+
+	return svgIcons
 }
--- transform_test.go
+++ transform_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/textproto"
 	"reflect"
+	"regexp"
 	"text/template"
 
 	. "github.com/onsi/ginkgo"
@@ -43,6 +44,53 @@ var _ = Describe("Transform", func() {
 		  </svg>`
 
 			Expect(result).To(Equal(expectedResult))
+		})
+	})
+
+	Describe("#exists", func() {
+		It("returns false if a folder doesn't exist", func() {
+			Expect(exists("/not/a/folder")).To(Equal(false))
+		})
+
+		It("returns true if a folder exists", func() {
+			Expect(exists(".")).To(Equal(true))
+		})
+	})
+
+	Describe("#fetchIcons", func() {
+		It("throws an exception if src folder doesn't exist", func() {
+			expect := func() {
+				fetchIcons("/not/a/folder")
+			}
+
+			Expect(expect).To(Panic())
+		})
+
+		It("returns a list of svg files", func() {
+			files := fetchIcons("./test-assets")
+
+			Expect(len(files)).To(Equal(2))
+		})
+	})
+
+	Describe("#extractSvgContent ", func() {
+		It("returns the expected content", func() {
+			var expectedResult = `<symbol id="icon-1" viewBox="0 0 1024 1024">
+				<title>icon 1</title>
+				<path class="path1" d="M512 32l-512 512 96 96 96-96v416h256v-192h128v192h256v-416l96 96 96-96-512-512zM512 448c-35.346 0-64-28.654-64-64s28.654-64 64-64c35.346 0 64 28.654 64 64s-28.654 64-64 64z"></path>
+			</symbol>
+			<symbol id="icon-2" viewBox="0 0 1024 1024">
+				<title>icon 2</title>
+				<path class="path1" d="M512 32l-512 512 96 96 96-96v416h256v-192h128v192h256v-416l96 96 96-96-512-512zM512 448c-35.346 0-64-28.654-64-64s28.654-64 64-64c35.346 0 64 28.654 64 64s-28.654 64-64 64z"></path>
+			</symbol>`
+
+			files := fetchIcons("./test-assets")
+
+			reg, _ := regexp.Compile("[\\s\t\n]")
+
+			result := extractSvgContent(files)
+
+			Expect(reg.ReplaceAllString(result, "")).To(Equal(reg.ReplaceAllString(expectedResult, "")))
 		})
 	})
 })
